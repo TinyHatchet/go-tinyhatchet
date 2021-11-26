9538f6b0524dba027db9dfd606417ba0bfc531d1
Landon Dyck <landon@codemonkeysoftware.net>
2021-11-26 16:12:39 -0600

    split out from main repo

---
+++ go.mod
@@ -0,0 +1,3 @@
+module git.codemonkeysoftware.net/mouseion/go_mousion
+
+go 1.17
---
+++ source.go
@@ -0,0 +1,67 @@
+package mouseion
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+type LogEntry struct {
+	Timestamp time.Time `json:"timestamp"`
+	Text      string    `json:"text"`
+	Tags      []string  `json:"tags"`
+}
+
+// Logger sends log messages to MouseionHost using HTTPClient. MouseionHost
+// is the only required field.
+type Logger struct {
+	HTTPClient   *http.Client
+	LogErrors    bool
+	MouseionHost string
+}
+
+func (logger *Logger) Print(v ...interface{}) {
+	logger.send(fmt.Sprint(v...))
+}
+
+func (logger *Logger) Printf(format string, v ...interface{}) {
+	logger.send(fmt.Sprintf(format, v...))
+}
+
+func (logger *Logger) Println(v ...interface{}) {
+	logger.Print(v...)
+}
+
+func (logger *Logger) send(text string) {
+	err := send(logger.HTTPClient, logger.MouseionHost, text)
+	if err != nil && logger.LogErrors {
+		log.Println(err)
+	}
+}
+
+func send(client *http.Client, host string, text string) error {
+	if client != nil {
+		client = http.DefaultClient
+	}
+	entry := LogEntry{Timestamp: time.Now(), Text: text, Tags: nil}
+	entryJSON, err := json.Marshal(entry)
+	if err != nil {
+		return err
+	}
+	resp, err := client.Post(jsonURL(host), "application/json", bytes.NewBuffer(entryJSON))
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		body, _ := ioutil.ReadAll(resp.Body)
+		return fmt.Errorf("%d %s", resp.StatusCode, body)
+	}
+	return nil
+}
+func jsonURL(host string) string {
+	return host + "/ingest.json"
+}
